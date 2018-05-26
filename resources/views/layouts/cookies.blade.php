689df2854134a80c0b9e511636ef54f0932ddb17
callyall <nikolay_brankov@outlook.com>
2018-05-26 20:04:40 +0300

    Add files via upload

---
+++ resources/views/layouts/cookies.blade.php
@@ -0,0 +1,19 @@
+<div id="cookies" style="position: fixed;bottom: 0;width: 100%;/*height: 50%;*/background: rgba(0,0,0,0.5);z-index: 99999;padding: 1%; max-height:100%; overflow: auto;" class="container-fluid">
+    <div class="row" style="padding:5%">
+        <h1 style="color:white" class="text-center">We use cookies.</h1>
+        <p  style="color:white">If you accept the use of cookies we'll use these:</p>
+        <ul>
+            <li  style="color:white"><span style="font-weight: bold">consent</span> - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at.</li>
+            <li  style="color:white"><span style="font-weight: bold">XSRF-TOKEN</span> - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at.</li>
+            <li  style="color:white"><span style="font-weight: bold">laravel_session</span> - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at.</li>
+            <li  style="color:white"><span style="font-weight: bold">Google analytics cookies</span> - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at.</li>
+        </ul>
+        <p style="font-weight: bold;color:white">You can always change your mind and deny the use of cookies by going to our <a href="/cookies">cookies</a> page.</p>
+        <table style="margin: auto">
+            <tr>
+                <td><button class="btn btn-info" onclick="accept()">I accept</button></td>
+                <td><button class="btn btn-danger" onclick="refuse()">I do not accept</button></td>
+            </tr>
+        </table>
+    </div>
+</div>
